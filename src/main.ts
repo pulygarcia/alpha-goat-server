7b1007e369c1624540cc66e711937d58001198f6
Puly Garcia <pulygarcia09@gmail.com>
2026-05-18 17:25:06 -0300

    fix(main): import cookie-parser as namespace to fix CJS default import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 
