f38570389242066d086fd6f4bdf4df6274ec6aca
Puly Garcia <pulygarcia09@gmail.com>
2026-05-25 15:47:30 -0300

    chore(bootstrap): log app and swagger URLs on startup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
@@ -29,6 +29,9 @@ async function bootstrap() {
 
   const port = Number(config.get<string>('PORT') ?? 3001);
   await app.listen(port);
+
+  Logger.log(`App running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log(`Swagger docs on http://localhost:${port}/docs`, 'Bootstrap');
 }
 
 void bootstrap();
